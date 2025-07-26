27206901133b85788d94fd86b000fe2944b90fd2
IamPrachu7 <iamprachu7@gmail.com>
2025-07-26 13:42:44 +0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the React app build folder
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handles any requests and sends back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
